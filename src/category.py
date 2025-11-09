0273cb2df2c84e6eb476485011f39c43467afe08
ildar-vk <kalimov.ildar.v@gmail.com>
2025-11-09 09:56:19 +0500

    код без тестов

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     category_count = 0
     product_count = 0
@@ -11,7 +14,21 @@ class Category:
 
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
-        Category.category_count += 1
-        Category.product_count += len(products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        self.product_count += 1
+
+    @property
+    def products(self) -> list:
+        products = []
+        for product in self.__products:
+            product_inform = f'{product.name}, {product.price}. Остаток: {self.product_count})'
+            products.append(product_inform)
+        return products
+
+
+
+
--- src/product.py
+++ src/product.py
@@ -7,5 +7,24 @@ class Product:
 
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_date):
+        name = product_date.get("name")
+        description = product_date.get("description")
+        price = product_date.get("price")
+        quantity = product_date.get("quantity")
+
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+    @price.setter
+    def price(self, price):
+        if price <=0:
+            print("Цена не должна быть нулевой или отрицательной")
+        else:
+            self.__price = price
